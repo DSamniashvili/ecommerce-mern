c158ab539e330c155102e256848c520bcfea1579
dea <dea@newrow.com>
2021-01-08 17:31:05 +0400

    added bootstrap theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/static-components/Header';
+import Footer from './components/static-components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       ecommerce
+      <Footer />
     </div>
   );
 }
